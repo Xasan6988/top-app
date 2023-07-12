f36dd753d61786c8deeb7d33d9955e0ace934534
Atryom Khabibyllin <238136@mail.ru>
2023-07-12 11:54:25 +0300

    feat fonts and fix styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import '/styles/globals.css';
 import React from 'react';
-
 
 const Home = (): JSX.Element => {
   return (
-    <>
+    <div>
       <p>This is root page</p>
-    </>
+    </div>
   );
 };
 
